43910768b80e04b28cf19135981377f53199d25b
John Cen <john.cen397@gmail.com>
2019-09-23 17:24:32 -0400

    Added success and Error responses and a scraping endpoint

--- api-handler.js
+++ api-handler.js
@@ -1,5 +1,52 @@
 'use strict';
+const axios = require('axios')
+const IndeedScraper = require('indeed-scraper')
 
-module.exports.scrapeIndeedJobs = async (event) => {
+//--------------- HELPER FUNCTIONS ---------------
+
+const sendSuccessResponse = (statusCode, OKmessage) => {
+    return {
+        status: statusCode,
+        headers: {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*"
+        },
+        body: JSON.stringify({ message: OKmessage })
+    }
+}
+
+const sendErrorResponse = (statusCode, ERRmessage) => {
+    return {
+        status: statusCode,
+        headers: {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*"
+        },
+        body: JSON.stringify({ error_message: ERRmessage })
+    }
+}
+
+//--------------- LAMBDA FUNCTIONS ---------------
+
+module.exports.scrapeIndeed = async (event) => {
+    let qs = event.queryStringParameters
+    let queryOptions = {
+        host: "www.indeed.ca",
+        query: 'Software Engineer',
+        city: 'Toronto, ON',
+        radius: '25',
+        level: 'entry_level',
+        jobType: 'fulltime',
+        maxAge: '7',
+        sort: 'date',
+        limit: 100
+    }
+    try {
+        let indeedResults = await IndeedScraper.query(queryOptions) //array of results
+        console.log('indeed results:\n', indeedResults)
+    } catch (err) {
+        console.error('scrape indeed caught err:', err.message)
+        return sendErrorResponse(400, err.message)
+    }
 
 };
